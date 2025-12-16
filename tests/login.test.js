3990b5363c4518edf8c494cb71753ff9f49eceac
Gleisson  Rodrigo <gleissonrodrigo@MacBook-Pro-de-Gleisson.local>
2025-12-16 15:23:26 -0300

    Add Vus and Duration time to execute tests

--- tests/login.test.js
+++ tests/login.test.js
@@ -5,9 +5,10 @@ import { check } from 'k6';
 
 export const options = {
 
-    iterations: 20,
+    vus: 10,
+    duration: '30s',
     thresholds: {
-        http_req_duration: ['p(90)<10','max<10'], // 95% of requests should be below 200ms
+        http_req_duration: ['p(90)<3000', 'max<5000'], // 95% of requests should be below 200ms
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     },
 };
