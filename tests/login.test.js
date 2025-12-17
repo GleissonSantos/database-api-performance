eaba62d4a786470d7c0c46d9341e0998c5bebb15
Gleisson  Rodrigo <gleissonrodrigo@MacBook-Pro-de-Gleisson.local>
2025-12-17 10:36:38 -0300

    Add fixture filed to improve the test quality

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,7 +1,7 @@
 import http from 'k6/http'
 import { sleep } from 'k6'
 import { check } from 'k6';
-
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
 
 export const options = {
 
@@ -22,11 +22,7 @@ export const options = {
 export default function () {
 
     const url = 'http://localhost:3000/login'
-    const payload = JSON.stringify({
-        username: 'julio.lima',
-        senha: '123456',
-    });
-
+    const payload = JSON.stringify(postLogin);
     const params = {
         headers: {
             'Content-Type': 'application/json',
